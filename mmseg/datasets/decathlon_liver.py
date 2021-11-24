0da39a7166b05c86c0e80e1877fb01a78ec8a545
david <david.hagerman.olzon@gmail.com>
2021-11-24 16:31:59 +0100

    Added support for Decathlon data.

---
+++ mmseg/datasets/decathlon_liver.py
@@ -0,0 +1,24 @@
+from .builder import DATASETS
+from .custom import CustomDataset
+
+
+@DATASETS.register_module()
+class DecathlonLiverDataset(CustomDataset):
+    """Decathlon Liver dataset.
+
+    In segmentation map annotation for Decathlon Liver, 0 stands for background.
+    ``reduce_zero_label`` is fixed to False.
+    The ``img_suffix`` is fixed to '.jpg' and ``seg_map_suffix`` is fixed to
+    '.png'.
+    """
+    CLASSES = (
+        'background', 'liver', 'cancer')
+
+    PALETTE = [[120, 120, 120], [180, 120, 120], [6, 230, 230]]
+
+    def __init__(self, **kwargs):
+        super(DecathlonLiverDataset, self).__init__(
+            img_suffix='.png',
+            seg_map_suffix='.png',
+            reduce_zero_label=False,
+            **kwargs)
