e53e15eda96fa4a75d7315658bd00cb075f01121
David Hagerman Olzon <olzond@chalmers.se>
2021-12-01 10:21:23 +0100

    Updated with lots of bugfixes for Swin finetuning on decathlon.

--- configs/_base_/datasets/decathlon_liver.py
+++ configs/_base_/datasets/decathlon_liver.py
@@ -1,23 +1,23 @@
 # dataset settings
-dataset_type = 'Decathlon_Liver'
-data_root = '/media/davidhagerman/SSD_4TB/david/Decathlon/2D/Task03_Liver'
+dataset_type = 'DecathlonLiverDataset'
+data_root = '/media/roman/SSD_4TB/david/Decathlon/2D/Task03_Liver'
 img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=False)
+    mean=[127.5], std=[63.75], to_rgb=False)
 crop_size = (224, 224)
 train_pipeline = [
-    dict(type='LoadImageFromFile'),
+    dict(type='LoadImageFromFile', color_type='grayscale'),
     dict(type='LoadAnnotations', reduce_zero_label=True),
-    #dict(type='Resize', img_scale=(2048, 512), ratio_range=(0.5, 2.0)),
+    dict(type='Resize', img_scale=(2048, 512), ratio_range=(0.5, 2.0)),
     dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
     dict(type='RandomFlip', prob=0.5),
-    dict(type='PhotoMetricDistortion'),
+    #dict(type='PhotoMetricDistortion'),
     dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size=crop_size, pad_val=0, seg_pad_val=255),
+    #dict(type='Pad', size=crop_size, pad_val=0, seg_pad_val=255),
     dict(type='DefaultFormatBundle'),
     dict(type='Collect', keys=['img', 'gt_semantic_seg']),
 ]
 test_pipeline = [
-    dict(type='LoadImageFromFile'),
+    dict(type='LoadImageFromFile', color_type='grayscale'),
     dict(
         type='MultiScaleFlipAug',
         img_scale=(2048, 512),
@@ -32,23 +32,17 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    samples_per_gpu=4,
-    workers_per_gpu=4,
+    samples_per_gpu=32,
+    workers_per_gpu=8,
     train=dict(
         type=dataset_type,
         data_root=data_root,
-        img_dir='images/training',
-        ann_dir='annotations/training',
+        img_dir='img_dir/train',
+        ann_dir='ann_dir/train',
         pipeline=train_pipeline),
     val=dict(
         type=dataset_type,
         data_root=data_root,
-        img_dir='images/validation',
-        ann_dir='annotations/validation',
-        pipeline=test_pipeline),
-    test=dict(
-        type=dataset_type,
-        data_root=data_root,
-        img_dir='images/validation',
-        ann_dir='annotations/validation',
+        img_dir='img_dir/val',
+        ann_dir='ann_dir/val',
         pipeline=test_pipeline))
--- mmseg/datasets/decathlon_liver.py
+++ mmseg/datasets/decathlon_liver.py
@@ -8,11 +8,10 @@ class DecathlonLiverDataset(CustomDataset):
 
     In segmentation map annotation for Decathlon Liver, 0 stands for background.
     ``reduce_zero_label`` is fixed to False.
-    The ``img_suffix`` is fixed to '.jpg' and ``seg_map_suffix`` is fixed to
+    The ``img_suffix`` is fixed to '.png' and ``seg_map_suffix`` is fixed to
     '.png'.
     """
-    CLASSES = (
-        'background', 'liver', 'cancer')
+    CLASSES = ('liver', 'cancer')
 
     PALETTE = [[120, 120, 120], [180, 120, 120], [6, 230, 230]]
 
@@ -20,5 +19,5 @@ class DecathlonLiverDataset(CustomDataset):
         super(DecathlonLiverDataset, self).__init__(
             img_suffix='.png',
             seg_map_suffix='.png',
-            reduce_zero_label=False,
+            reduce_zero_label=True,
             **kwargs)
