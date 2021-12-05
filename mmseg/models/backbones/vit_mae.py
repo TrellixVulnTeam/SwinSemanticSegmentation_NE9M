0196e650f99c65b6d00a5153559aa4f1dcd05b8b
Eiphodos <david.hagerman.olzon@gmail.com>
2021-12-05 20:10:49 +0100

    Added support for pretrained weights in the same format that MMCV encoder/decoder expects.

--- mmseg/models/backbones/vit_mae.py
+++ mmseg/models/backbones/vit_mae.py
@@ -4,6 +4,10 @@ from torch import nn
 from einops import rearrange, repeat
 from einops.layers.torch import Rearrange
 
+from mmcv_custom import load_checkpoint
+from mmseg.utils import get_root_logger
+from ..builder import BACKBONES
+
 # helpers
 
 def pair(t):
@@ -109,6 +113,32 @@ class ViTMAE(nn.Module):
             nn.LayerNorm(dim),
             nn.Linear(dim, num_classes)
         )
+
+    def init_weights(self, pretrained=None):
+        """Initialize the weights in backbone.
+
+        Args:
+            pretrained (str, optional): Path to pre-trained weights.
+                Defaults to None.
+        """
+
+        def _init_weights(m):
+            if isinstance(m, nn.Linear):
+                trunc_normal_(m.weight, std=.02)
+                if isinstance(m, nn.Linear) and m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.LayerNorm):
+                nn.init.constant_(m.bias, 0)
+                nn.init.constant_(m.weight, 1.0)
+
+        if isinstance(pretrained, str):
+            self.apply(_init_weights)
+            logger = get_root_logger()
+            load_checkpoint(self, pretrained, strict=False, logger=logger)
+        elif pretrained is None:
+            self.apply(_init_weights)
+        else:
+            raise TypeError('pretrained must be a str or None')
 
     def forward(self, img):
         x = self.to_patch_embedding(img)
