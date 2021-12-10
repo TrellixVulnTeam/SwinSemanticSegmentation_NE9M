275f6a92913a89c2253d44e45f67837b4f359871
Eiphodos <david.hagerman.olzon@gmail.com>
2021-12-10 09:58:21 +0100

    Fix for image size (should not be a list of size 1).

--- mmseg/models/backbones/vit_esvit.py
+++ mmseg/models/backbones/vit_esvit.py
@@ -152,14 +152,14 @@ class PatchEmbed(nn.Module):
 class EsViT(nn.Module):
     """ Vision Transformer """
 
-    def __init__(self, img_size=[224], patch_size=16, in_chans=3, num_classes=0, embed_dim=768, depth=12,
+    def __init__(self, img_size=224, patch_size=16, in_chans=3, num_classes=0, embed_dim=768, depth=12,
                  num_heads=12, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop_rate=0., attn_drop_rate=0.,
                  drop_path_rate=0., norm_layer=nn.LayerNorm, out_indices=[3, 5, 7, 11], **kwargs):
         super().__init__()
         self.num_features = self.embed_dim = embed_dim
 
         self.patch_embed = PatchEmbed(
-            img_size=img_size[0], patch_size=patch_size, in_chans=in_chans, embed_dim=embed_dim)
+            img_size=img_size, patch_size=patch_size, in_chans=in_chans, embed_dim=embed_dim)
         num_patches = self.patch_embed.num_patches
 
         self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
