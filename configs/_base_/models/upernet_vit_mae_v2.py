254840b534812fc41901347bb766276530873165
Eiphodos <david.hagerman.olzon@gmail.com>
2022-01-10 22:13:36 +0100

    Added support for Original MAE weights (using BEiT's ViT code)

---
+++ configs/_base_/models/upernet_vit_mae_v2.py
@@ -0,0 +1,48 @@
+# model settings
+norm_cfg = dict(type='SyncBN', requires_grad=True)
+model = dict(
+    type='EncoderDecoder',
+    pretrained=None,
+    backbone=dict(
+        type='BEiT',
+        patch_size=16,
+        embed_dim=768,
+        depth=12,
+        num_heads=12,
+        mlp_ratio=4.,
+        qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6),
+        img_size=224,
+        channels=3,
+        num_classes=1000,
+        out_indices=[3, 5, 7, 11],
+        use_abs_pos_emb=True,
+        use_rel_pos_emb=True),
+    decode_head=dict(
+        type='UPerHead',
+        in_channels=[768, 768, 768, 768],
+        in_index=[0, 1, 2, 3],
+        pool_scales=(1, 2, 3, 6),
+        channels=768,
+        dropout_ratio=0.1,
+        num_classes=3,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
+    auxiliary_head=dict(
+        type='FCNHead',
+        in_channels=768,
+        in_index=2,
+        channels=256,
+        num_convs=1,
+        concat_input=False,
+        dropout_ratio=0.1,
+        num_classes=3,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.4)),
+    # model training and testing settings
+    train_cfg=dict(),
+    test_cfg=dict(mode='whole'))
---
+++ tools/log_results_ade.py
@@ -0,0 +1,31 @@
+import neptune.new as neptune
+import json
+import argparse
+
+parser = argparse.ArgumentParser(description='Log to Neptune')
+parser.add_argument('logfile', help='Path to mmseg json log file')
+args = parser.parse_args()
+
+with open(args.logfile, 'r') as file:
+    results = file.readlines()
+    results = [line.rstrip() for line in results]
+
+run = neptune.init(
+    project='eiphodos/Asparagus'
+)
+
+for r in results[1:]:
+    stats = eval(r)
+    iter = stats['iter']
+    if not iter % 1000:
+        if stats['mode'] == 'train':
+            run['train/decode_loss_seg'].log(stats['decode.loss_seg'], iter)
+            run['train/decode_acc_seg'].log(stats['decode.acc_seg'], iter)
+            run['train/aux_loss_seg'].log(stats['aux.loss_seg'], iter)
+            run['train/aux_acc_seg'].log(stats['aux.acc_seg'], iter)
+            run['train/loss'].log(stats['loss'], iter)
+        if stats['mode'] == 'val':
+            run['val/mIoU'].log(stats['mIoU'], iter)
+            run['val/mDice'].log(stats['mDice'], iter)
+            run['val/mAcc'].log(stats['mAcc'], iter)
+            run['val/aAcc'].log(stats['aAcc'], iter)
