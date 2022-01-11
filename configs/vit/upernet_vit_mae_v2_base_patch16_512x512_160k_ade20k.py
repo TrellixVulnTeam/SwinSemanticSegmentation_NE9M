f4632bf0ca4a41bf8219f551fd04c8e9b841e73a
Eiphodos <david.hagerman.olzon@gmail.com>
2022-01-11 14:50:06 +0100

    Removed SETRResize and flip from eval, added the LayerDecayOptimizer constructor to remove weight decay from all bias/pos_emb, removed absolute positional embeddings

--- configs/vit/upernet_vit_mae_v2_base_patch16_512x512_160k_ade20k.py
+++ configs/vit/upernet_vit_mae_v2_base_patch16_512x512_160k_ade20k.py
@@ -14,7 +14,7 @@ model = dict(
         patch_size=16,
         in_chans=3,
         num_classes=1000,
-        use_abs_pos_emb=True,
+        use_abs_pos_emb=False,
         use_rel_pos_bias=True
     ),
     decode_head=dict(
@@ -30,10 +30,14 @@ model = dict(
 )
 
 # AdamW optimizer, no weight decay for position embedding & layer norm in backbone
-optimizer = dict(_delete_=True, type='AdamW', lr=0.00006, betas=(0.9, 0.999), weight_decay=0.01,
-                 paramwise_cfg=dict(custom_keys={'absolute_pos_embed': dict(decay_mult=0.),
-                                                 'relative_position_bias_table': dict(decay_mult=0.),
-                                                 'norm': dict(decay_mult=0.)}))
+#optimizer = dict(_delete_=True, type='AdamW', lr=0.00006, betas=(0.9, 0.999), weight_decay=0.01,
+#                 paramwise_cfg=dict(custom_keys={'absolute_pos_embed': dict(decay_mult=0.),
+#                                                 'relative_position_bias_table': dict(decay_mult=0.),
+#                                                 'norm': dict(decay_mult=0.)}))
+
+optimizer = dict(_delete_=True, type='AdamW', lr=7e-4, betas=(0.9, 0.999), weight_decay=0.05,
+                 constructor='LayerDecayOptimizerConstructor',
+                 paramwise_cfg=dict(num_layers=12, layer_decay_rate=0.65))
 
 lr_config = dict(_delete_=True, policy='poly',
                  warmup='linear',
@@ -63,11 +67,10 @@ test_pipeline = [
     dict(
         type='MultiScaleFlipAug',
         img_scale=(2048, 512),
-        img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
-        flip=True,
+        #img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
+        flip=False,
         transforms=[
-            dict(type='SETR_Resize', keep_ratio=True,
-                 crop_size=crop_size, setr_multi_scale=True),
+            dict(type='Resize', keep_ratio=True),
             dict(type='RandomFlip'),
             dict(type='Normalize', **img_norm_cfg),
             dict(type='ImageToTensor', keys=['img']),
