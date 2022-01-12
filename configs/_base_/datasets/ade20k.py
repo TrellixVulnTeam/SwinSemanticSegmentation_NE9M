cbc8d6b045bc18ad2b45d16e5480deedcc3a0b48
Eiphodos <david.hagerman.olzon@gmail.com>
2022-01-12 13:37:42 +0100

    Added more original BEiT configs.

--- configs/_base_/datasets/ade20k.py
+++ configs/_base_/datasets/ade20k.py
@@ -3,7 +3,7 @@ dataset_type = 'ADE20KDataset'
 data_root = 'data/ADE20K'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-crop_size = (224, 224)
+crop_size = (512, 512)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', reduce_zero_label=True),
@@ -18,11 +18,18 @@ train_pipeline = [
 ]
 test_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='Resize', img_scale=crop_size),
-    dict(type='RandomFlip', prob=0.0),
-    dict(type='Normalize', **img_norm_cfg),
-    dict(type='ImageToTensor', keys=['img']),
-    dict(type='Collect', keys=['img'])
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(2048, 512),
+        # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
 ]
 data = dict(
     samples_per_gpu=4,
