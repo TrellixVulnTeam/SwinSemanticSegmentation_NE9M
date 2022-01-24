fc97239aa0f3ebb2ded33875d455e69cace91492
Eiphodos <david.hagerman.olzon@gmail.com>
2022-01-24 12:11:05 +0100

    Forgot the argument for RandomRotate.

--- configs/_base_/datasets/decathlon_liver.py
+++ configs/_base_/datasets/decathlon_liver.py
@@ -15,7 +15,7 @@ train_pipeline = [
     dict(type='Resize', img_scale=(1024, 512), ratio_range=(0.5, 2.0)),
     dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
     dict(type='RandomFlip', prob=0.5),
-    dict(type='RandomRotate', prob=0.2),
+    dict(type='RandomRotate', prob=0.25, degree=36),
     dict(type='PhotoMetricDistortion'),
     dict(type='Normalize', **img_norm_cfg),
     #dict(type='Pad', size=crop_size, pad_val=0, seg_pad_val=255),
